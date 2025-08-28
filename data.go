7f9875c06ecadd50839ecc48622bbfbbc645172e
Valentin Kuznetsov <vkuznet@gmail.com>
2025-08-28 13:42:25 -0400

    expand request record struct and provide RequestRecord public API

--- data.go
+++ data.go
@@ -1,18 +1,40 @@
 package main
 
-const (
-	DoesNotExist = iota
-	Accepted
-	InProgress
-	SyncMetadata
-	SyncProvenance
-	Completed
+import (
+	"github.com/google/uuid"
 )
 
+const (
+	DoesNotExist      = iota + 100 // 100 does not exit
+	IncompleteRequest              // 101 incomplete request
+	UnsupportedMethod              // 102 unsupported method
+	InsertError                    // 103 insert error
+	Accepted                       // 104 request accepted
+	InProgress                     // 105 request in progress
+	SyncMetadata                   // 106 synching metadata
+	SyncProvenance                 // 107 synching provenance records
+	Completed                      // 108 request completed
+)
+
+// RequestData represents SyncService request data payload
 type RequestData struct {
-	UID        string `json:"uuid"`
-	SourceURL  string `json:"source_url"`
-	TargetURL  string `json:"target_url"`
-	Status     string `json:"status"`
-	StatusCode int    `json:"status_code"`
+	UUID        string `json:"uuid"`
+	SourceURL   string `json:"source_url"`
+	SourceToken string `json:"source_token"`
+	TargetURL   string `json:"target_url"`
+	TargetToken string `json:"target_token"`
+	Status      string `json:"status"`
+	StatusCode  int    `json:"status_code"`
+}
+
+// ReqeuestRecord creates new request record for database out of payload
+func (p *RequestData) RequestRecord() map[string]any {
+	rec := make(map[string]any)
+	rec["source_url"] = p.SourceURL
+	rec["target_url"] = p.TargetURL
+	rec["source_token"] = p.SourceToken
+	rec["target_token"] = p.TargetToken
+	uuid, _ := uuid.NewRandom()
+	rec["uuid"] = uuid
+	return rec
 }
